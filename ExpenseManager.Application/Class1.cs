bb40175ec69ab3611675697436ad4a406e91e2b3
Hamid Seta <setahamid@gmail.com>
2018-06-03 07:51:42 +0530

    Initial commit for Expense Manager

---
+++ ExpenseManager.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExpenseManager.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExpenseManager.Infrastructure.EntiryFramework/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExpenseManager.Infrastructure.EntiryFramework
+{
+    public class Class1
+    {
+    }
+}
